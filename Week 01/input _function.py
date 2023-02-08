65a22f01c884953cdb05efb440b9334cfbc659e9
Nihal Abbas <nihal.abbas@studytafensw.edu.au>
2023-02-08 16:52:59 +1100

    Initial commit

---
+++ Week 01/input _function.py
@@ -0,0 +1,9 @@
+name = ""
+age = input("Please enter your name: ")
+
+# String must be converted to a number  (integer) to be used for calculations
+age = int(input("Please enter your age: "))
+
+# Test
+print("Hello", name, "you are", age, "next year you'll be", age + 1)
+
